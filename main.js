90deede3025d89d3be56b567ac7bbad49173ae0b
muha <1student.muhammed1@gmail.com>
2023-05-31 00:52:57 +0300

    feat : repair function for toggle switch

--- main.js
+++ main.js
@@ -1,22 +1,16 @@
 let great = document.querySelector('.great');
 let body = document.querySelector('.body');
-let btn = document.querySelector('.btn');
+let icon = document.querySelector('.icon');
+let checkbox = document.querySelector('.checkbox');
 
-btn.addEventListener('click', function () {
-    if (great.textContent === "White Mood") {
+checkbox.addEventListener('change', function () {
+    if (checkbox.checked) {
         great.textContent = "Dark Mood";
         great.style.color = 'white';
-        body.style.backgroundColor = '#262626';
-        btn.textContent = "Switch to White mood";
-        btn.classList.add("btn-white");
-        btn.classList.remove("btn-dark");
-
+        body.classList.add('dark-mode');
     } else {
         great.textContent = "White Mood";
         great.style.color = '';
-        body.style.backgroundColor = '';
-        btn.textContent = "Switch to Dark mood";
-        btn.classList.remove("btn-white");
-        btn.classList.add("btn-dark");
+        body.classList.remove('dark-mode');
     }
 });
